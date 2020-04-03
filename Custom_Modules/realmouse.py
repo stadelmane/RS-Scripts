5a0f3d97f7c19dad9e26bc0cb246f5e3c3af7fba
Eric Stadelman <ewstadelman@gmail.com>
2020-04-03 12:54:25 -0500

    Speed up mouse since VM lags a bit

--- Custom_Modules/realmouse.py
+++ Custom_Modules/realmouse.py
@@ -23,7 +23,7 @@ def move_mouse_to(x, y):
 			# calculates the distance from current position to target position
 			distance = int(((x - curr_x)**2 + (y - curr_y)**2)**0.5)
 			# calculates a random time to make the move take based on the distance
-			duration_of_move = (distance * random.random() / 2000) + 0.5
+			duration_of_move = (distance * random.random() / 2000) + 0.4
 			# move the mouse to our position and takes the time of our duration just
 			# calculated
 			pyautogui.moveTo(x, y, duration_of_move, pyautogui.easeInOutQuad)
